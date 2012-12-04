9cf88335bafc657d17921aa0a918b6a48ea552fa
gaffu <>
2012-12-04 16:53:06 +0100

    Changed benchmark post name to bqid

--- application/views/admin/benchmark/index.php
+++ application/views/admin/benchmark/index.php
@@ -1,4 +1,7 @@
+<form action="<?php echo $this->createUrl("admin/benchmark/generaterapport/surveyid/$surveyid"); ?>" method="post">
 <?php 
     echo CHtml::dropDownList('language', null, $langauges);
-    echo CHtml::dropDownList('question', null, $questions);
+    echo CHtml::dropDownList('bqid', null, $questions);
 ?>
+    <input type="submit" value="<?php $clang->eT('Generate'); ?>" />
+</form>
