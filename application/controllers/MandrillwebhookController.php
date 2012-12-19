05f6e833ae9696a7baf44414cac649359b5d9c57
gaffu <jp@jposs.dk>
2012-12-19 10:02:02 +0100

    Changed tags check, apparently mandrill does not guarantee they will appear in same order.

--- application/controllers/MandrillwebhookController.php
+++ application/controllers/MandrillwebhookController.php
@@ -16,29 +16,36 @@ class MandrillWebHookController extends LSYii_Controller {
             $post = json_decode($_POST['mandrill_events'], true);
             $post = $post[0];
             // Make sure it is a webhook intended for Limesurvey
-            if (isset($post['msg']['tags']) && $post['msg']['tags'][0] == 'limesurvey' && substr($post['msg']['tags'][1], 0, 6) == 'Survey') {
-                $surveyId = substr($post['msg']['tags'][1], 7);
+            if (isset($post['msg']['tags'])) {
+                if ($post['msg']['tags'][0] == 'limesurvey' || substr($post['msg']['tags'][0], 0, 6) == 'Survey') {
+                    if ($post['msg']['tags'][1] == 'limesurvey' || substr($post['msg']['tags'][1], 0, 6) == 'Survey') {
 
-                // Criteria for the token that the post was intended for
-                $criteria = new CDbCriteria();
-                $criteria->condition = 'email = "' . $post['msg']['email'] . '"';
-                $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
-                $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
-                
-                // Add new post request to the emailhistory array
-                $emailhistory[$post['msg']['ts']][] = $post;
-                // sort it so the newest is at the start of the array
-                krsort($emailhistory);                
-                foreach($emailhistory as $history){
-                    // Set the latest post request as the email status
-                    $tokenRow->setAttribute('emailstatus', $history[0]['event']);
-                    break;
+                        $surveyId = substr($post['msg']['tags'][1], 7);
+
+                        // Criteria for the token that the post was intended for
+                        $criteria = new CDbCriteria();
+                        $criteria->condition = 'email = "' . $post['msg']['email'] . '"';
+                        $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
+                        $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
+
+                        // Add new post request to the emailhistory array
+                        $emailhistory[$post['msg']['ts']][] = $post;
+                        // sort it so the newest is at the start of the array
+                        krsort($emailhistory);
+                        foreach ($emailhistory as $history) {
+                            // Set the latest post request as the email status
+                            $tokenRow->setAttribute('emailstatus', $history[0]['event']);
+                            break;
+                        }
+                        $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
+                        $tokenRow->save();
+                        return;
+                    }
                 }
-                $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
-                $tokenRow->save();
-                return;
             }
         }
     }
+
 }
+
 ?>
