735c67a00a11e2024935d4d2e5e015ca004991b2
gaffu <jp@jposs.dk>
2013-01-02 11:49:39 +0100

    Added loop to process bulk post request from Mandrill

--- application/controllers/MandrillwebhookController.php
+++ application/controllers/MandrillwebhookController.php
@@ -15,43 +15,45 @@ class MandrillWebHookController extends LSYii_Controller {
 //        file_put_contents($logPath.'/MandrillInfo.log', print_r($_POST, true)."\n", FILE_APPEND);
         if (isset($_POST['mandrill_events'])) {
             // Decode the value as a json object
-            $post = json_decode($_POST['mandrill_events'], true);
-            $post = $post[0];
-            // Make sure it is a webhook intended for Limesurvey
-            if (isset($post['msg']['tags'])) {
-                if ($post['msg']['tags'][0] == 'limesurvey' || substr($post['msg']['tags'][0], 0, 6) == 'Survey') {
-                    if ($post['msg']['tags'][1] == 'limesurvey' || substr($post['msg']['tags'][1], 0, 6) == 'Survey') {
-                        if ($post['msg']['tags'][0] == 'limesurvey') {
-                            $surveyId = substr($post['msg']['tags'][1], 7);
-                        } else {
-                            $surveyId = substr($post['msg']['tags'][0], 7);
-                        }
+            $posts = json_decode($_POST['mandrill_events'], true);
+            foreach ($posts as $post) {
+                try {
+                    // Make sure it is a webhook intended for Limesurvey
+                    if (isset($post['msg']['tags'])) {
+                        if ($post['msg']['tags'][0] == 'limesurvey' || substr($post['msg']['tags'][0], 0, 6) == 'Survey') {
+                            if ($post['msg']['tags'][1] == 'limesurvey' || substr($post['msg']['tags'][1], 0, 6) == 'Survey') {
+                                if ($post['msg']['tags'][0] == 'limesurvey') {
+                                    $surveyId = substr($post['msg']['tags'][1], 7);
+                                } else {
+                                    $surveyId = substr($post['msg']['tags'][0], 7);
+                                }
 
-                        // Criteria for the token that the post was intended for
-                        try {
-                            $criteria = new CDbCriteria();
-                            $criteria->condition = 'tid = "' . $post['msg']['metadata']['tid'] . '"';
-                            $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
-                            $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
-                            // Add new post request to the emailhistory array
-                            $emailhistory[$post['ts']][] = $post;
-                            // sort it so the newest is at the start of the array
-                            krsort($emailhistory);
-                            foreach ($emailhistory as $history) {
-                                // Set the latest post request as the email status
-                                $tokenRow->setAttribute('emailstatus', $history[0]['event']);
-                                break;
+                                // Criteria for the token that the post was intended for
+                                $criteria = new CDbCriteria();
+                                $criteria->condition = 'tid = "' . $post['msg']['metadata']['tid'] . '"';
+                                $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
+                                $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
+                                // Add new post request to the emailhistory array
+                                $emailhistory[$post['ts']][] = $post;
+                                // sort it so the newest is at the start of the array
+                                krsort($emailhistory);
+                                foreach ($emailhistory as $history) {
+                                    // Set the latest post request as the email status
+                                    $tokenRow->setAttribute('emailstatus', $history[0]['event']);
+                                    break;
+                                }
+                                $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
+                                $tokenRow->save();
                             }
-                            $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
-                            $tokenRow->save();
-                            return;
-                        } catch (Exception $e) {
-                            file_put_contents($logPath.'/MandrillError.log', $e->getMessage()."\n", FILE_APPEND);      
                         }
                     }
+                } catch (Exception $e) {
+                    file_put_contents($logPath . '/MandrillError.log', $e->getMessage() . "\n", FILE_APPEND);
                 }
             }
         }
     }
+
 }
+
 ?>
