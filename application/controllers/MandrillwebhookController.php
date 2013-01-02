9beccbefb2bfd8a7d0b0e52990a120b8db85cd2d
gaffu <jp@jposs.dk>
2013-01-02 11:15:12 +0100

    Added some log and exception catching

--- application/controllers/MandrillwebhookController.php
+++ application/controllers/MandrillwebhookController.php
@@ -11,12 +11,14 @@ class MandrillWebHookController extends LSYii_Controller {
      */
     public function actionWebhook() {
         // Check if post has correct name
+        $logPath = Yii::app()->getRuntimePath();
+//        file_put_contents($logPath.'/MandrillInfo.log', print_r($_POST, true)."\n", FILE_APPEND);
         if (isset($_POST['mandrill_events'])) {
             // Decode the value as a json object
             $post = json_decode($_POST['mandrill_events'], true);
             $post = $post[0];
             // Make sure it is a webhook intended for Limesurvey
-            if (isset($post['msg']['tags'])) {                
+            if (isset($post['msg']['tags'])) {
                 if ($post['msg']['tags'][0] == 'limesurvey' || substr($post['msg']['tags'][0], 0, 6) == 'Survey') {
                     if ($post['msg']['tags'][1] == 'limesurvey' || substr($post['msg']['tags'][1], 0, 6) == 'Survey') {
                         if ($post['msg']['tags'][0] == 'limesurvey') {
@@ -26,29 +28,30 @@ class MandrillWebHookController extends LSYii_Controller {
                         }
 
                         // Criteria for the token that the post was intended for
-                        $criteria = new CDbCriteria();
-                        $criteria->condition = 'email = "' . $post['msg']['email'] . '"';
-                        $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
-                        $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
-
-                        // Add new post request to the emailhistory array
-                        $emailhistory[$post['ts']][] = $post;
-                        // sort it so the newest is at the start of the array
-                        krsort($emailhistory);
-                        foreach ($emailhistory as $history) {
-                            // Set the latest post request as the email status
-                            $tokenRow->setAttribute('emailstatus', $history[0]['event']);
-                            break;
+                        try {
+                            $criteria = new CDbCriteria();
+                            $criteria->condition = 'email = "' . $post['msg']['email'] . '"';
+                            $tokenRow = Tokens_dynamic::model($surveyId)->find($criteria);
+                            $emailhistory = unserialize($tokenRow->getAttribute('emailhistory'));
+                            // Add new post request to the emailhistory array
+                            $emailhistory[$post['ts']][] = $post;
+                            // sort it so the newest is at the start of the array
+                            krsort($emailhistory);
+                            foreach ($emailhistory as $history) {
+                                // Set the latest post request as the email status
+                                $tokenRow->setAttribute('emailstatus', $history[0]['event']);
+                                break;
+                            }
+                            $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
+                            $tokenRow->save();
+                            return;
+                        } catch (Exception $e) {
+                            file_put_contents($logPath.'/MandrillError.log', $e->getMessage()."\n", FILE_APPEND);      
                         }
-                        $tokenRow->setAttribute('emailhistory', serialize($emailhistory));
-                        $tokenRow->save();
-                        return;
                     }
                 }
             }
         }
     }
-
 }
-
 ?>
